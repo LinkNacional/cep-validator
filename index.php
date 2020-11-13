bb660005b93bcb314d44d152e65a06e60b688333
emanuellopess <lopes.emanuel@linknacional.com>
2020-11-13 16:07:24 -0300

    Verificacao dos intervalos alterada

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 
 //EXAMPLE
-/*
-$obj = new Cep('059999-98');
-$obj->setIntervalo([01.000001, 05.999999, 08.000000, 08.499999, 09.000001, 09.299999, 09.600001, 09.899999, 09.500001, 09.599999, 07.000001, 07.399999, 07.400001, 07.499999, 06.700001, 06.729999]);
+
+$obj = new Cep('09399999');
+$obj->setIntervalo(['01000001', '05999999',
+    '08000000', '08599999',
+    '09000001', '09399999',
+    '09600001', '09899999',
+    '09500001', '09599999',
+    '07000001', '07399999',
+    '07400001', '07499999',
+    '06700001', '06729999', ]);
 
 if ($obj->verificarCepNoIntervalo()) {
     echo 'é valido';
 } else {
     echo 'não é válido';
 }
-*/
 
 class Cep
 {
@@ -30,15 +36,41 @@ class Cep
     }
 
     /**
-     * Set the value of intervalo cep.
+     * Set the value of $cepIntervalo.
      */
     public function setIntervalo(array $intervalo)
     {
+        //Sanitizes and transform array type string to int
+        for ($c = 0; $c < count($intervalo); ++$c) {
+            $string = (string) $intervalo[$c];
+            $string = preg_replace('/[^0-9]/', '', $intervalo[$c]);
+            $intervalo[$c] = (int) $string;
+        }
+
         return $this->cepIntervalo = $intervalo;
     }
 
-     /**
-     * Get the value of getIntervalo
+    /**
+     * This function verifies if the $cep(String) exists in a determined static interval set in the array $cepValido.
+     * It checks each pair.
+     *
+     * @param mixed $cep
+     */
+    public function verificarCepNoIntervalo()
+    {
+        //Loop for checking the entire array
+        $intervalos = $this->getIntervalo();
+        for ($c = 0; $c < count($intervalos); $c += 2) {
+            if ($this->getCep() >= $intervalos[$c] && $this->getCep() <= $intervalos[$c + 1]) {
+                $this->valido = true;
+            }
+        }
+
+        return $this->valido;
+    }
+
+    /**
+     * Get the value of getIntervalo.
      */
     private function getIntervalo()
     {
@@ -61,35 +93,13 @@ class Cep
      * @return self
      */
     private function setCep($cep)
-    {   
+    {
         //Attribute sanitizer and validation
         if (null != $cep || $cep <= 0) {
             $string = (string) $cep;
             $string = preg_replace('/[^0-9]/', '', $cep);
-            $string = str_split($string);
-            array_splice($string, 2, 0, '.');
-            $string = implode('', $string);
-            $cep = (float) $string;
+            $cep = (int) $string;
         }
         $this->cep = $cep;
-    }
-
-    /**
-     * This function verifies if the $cep(String) exists in a determined static interval set in the array $cepValido.
-     * It checks each pair.
-     *
-     * @param mixed $cep
-     */
-    public function verificarCepNoIntervalo()
-    {
-        //Loop for checking the entire array
-        $intervalos = $this->getIntervalo();
-        for ($c = 0; $c < count($intervalos); $c += 2) {
-            if ($this->getCep() >= $intervalos[$c] && $this->getCep() <= $intervalos[$c + 1]) {
-                $this->valido = true;
-            }
-        }
-
-        return $this->valido;
     }
 }
