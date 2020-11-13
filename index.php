15f2a1738895805bacfdb6897abb21f817e637c2
souzadavi <contato@linknacional.com.br>
2020-11-13 14:34:09 -0300

    ajustando a classe

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
-//For debug purposes
-$obj = new verifica();
+//EXAMPLE
+/*
+$obj = new Cep('059999-98');
 
-$valido = $obj->verificaCep('01.29#3@8-9');
-
-if ($valido) {
+if ($obj->verificaCep()) {
     echo 'é valido';
 } else {
     echo 'não é válido';
 }
+*/
 
-class verifica
+class Cep
 {
-    private $cep = 0;
-
     private $cepValido = [01.000001, 05.999999, 08.000000, 08.499999, 09.000001, 09.299999, 09.600001, 09.899999, 09.500001, 09.599999, 07.000001, 07.399999, 07.400001, 07.499999, 06.700001, 06.729999];
     private $valido = false;
+    private $cep;
 
     /**
      * Constructor of class verifica.
@@ -24,38 +23,16 @@ class verifica
      * @param mixed $cep
      * @param mixed $cepValido
      */
-    public function contructorVerifica($cep, $cepValido)
+    public function __construct(string $cep)
     {
-        $this->cep = $cep;
-        $this->cepValido = $cepValido;
-    }
-
-    /**
-     * Get the value of cepValido.
-     */
-    public function getCepValido()
-    {
-        return $this->cepValido;
-    }
-
-    /**
-     * Set the value of cepValido.
-     *
-     * @param mixed $cepValido
-     *
-     * @return self
-     */
-    public function setCepValido($cepValido)
-    {
-        $this->cepValido = $cepValido;
-
-        return $this;
+        echo $cep;
+        $this->setCep($cep);
     }
 
     /**
      * Get the value of cep.
      */
-    public function getCep()
+    private function getCep()
     {
         return $this->cep;
     }
@@ -67,30 +44,8 @@ class verifica
      *
      * @return self
      */
-    public function setCep($cep)
-    {
-        $this->cep = $cep;
-
-        return $this;
-    }
-
-    /**
-     * This function verifies if the $cep(String) exists in a determined static interval set in the array $cepValido.
-     * It checks each pair.
-     *
-     * @param mixed $cep
-     */
-    public function verificaCep($cep)
-    {
-        //Verifies if cep is a string
-
-        try {
-            if ('string' != gettype($cep)) {
-                throw new Exception("{$cep} is not a String");
-            }
-        } catch (Exception $e) {
-            echo 'Exceção capturada: ',  $e->getMessage(), "\n";
-        }
+    private function setCep($cep)
+    {   
         //Attribute sanitizer and validation
         if (null != $cep || $cep <= 0) {
             $string = (string) $cep;
@@ -100,10 +55,22 @@ class verifica
             $string = implode('', $string);
             $cep = (float) $string;
         }
+        $this->cep = $cep;
+    }
 
+    /**
+     * This function verifies if the $cep(String) exists in a determined static interval set in the array $cepValido.
+     * It checks each pair.
+     *
+     * @param mixed $cep
+     */
+    public function verificaCep()
+    {
+    echo "Get". $this->getCep();
+    
         //Loop for checking the entire array
         for ($c = 0; $c < count($this->cepValido); $c += 2) {
-            if ($cep >= $this->cepValido[$c] && $cep <= $this->cepValido[$c + 1]) {
+            if ($this->getCep() >= $this->cepValido[$c] && $this->getCep() <= $this->cepValido[$c + 1]) {
                 $this->valido = true;
             }
         }
